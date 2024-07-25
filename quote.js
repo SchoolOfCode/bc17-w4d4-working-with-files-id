781b0585baab2dd83d9ca51d4445c652076ada42
dom-m17 <dominicmaynard00@gmail.com>
2024-07-25 14:30:01 +0100

    adding addQuote and getQuotes functions

--- quote.js
+++ quote.js
@@ -4,11 +4,30 @@ import { v4 as uuidv4 } from "uuid";
 const fileName = "quotes.json";
 
 export async function getQuotes() {
-
+    try {
+        const data = await fs.readFile(fileName, 'utf-8')
+        return data
+    } catch (error) {
+        return "Error"
+    }
 }
 
-export async function addQuote(quoteText) {
+const quoteText = "This is some random quote test";
+const result = await addQuote(quoteText);
+const result2 = await addQuote(quoteText);
+console.log(await getQuotes())
 
+export async function addQuote(quoteText) {
+    const randId = uuidv4()
+    const quoteObject = {
+        "textId" : randId,
+        "text" :  quoteText
+     }
+    const data = await getQuotes()
+    const quotes = JSON.parse(data);
+    quotes.push(quoteObject)
+    const quotesString = JSON.stringify(quotes)
+    const promise = await fs.writeFile("quotes.json", quotesString);
 }
 
 export async function getRandomQuote() {
